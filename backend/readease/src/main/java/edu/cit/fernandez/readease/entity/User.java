8fd4baecb6a2272d04a9b04ceb04e0eafe4d523b
DoctorGian <fernandezgian930@gmail.com>
2026-04-05 15:25:45 +0800

    Step 3: Upgrade Java Version - Compile: SUCCESS

--- backend/readease/src/main/java/edu/cit/fernandez/readease/entity/User.java
+++ backend/readease/src/main/java/edu/cit/fernandez/readease/entity/User.java
@@ -39,4 +39,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
